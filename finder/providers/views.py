d766ce266478f4fa5d9762dd9594addcbfcd5e65
Ali <alizahir@live.com>
2024-04-13 23:52:06 -0400

    add restaurant finder view with query params

--- finder/providers/views.py
+++ finder/providers/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+import os
+import requests
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import RetrieveModelMixin
+from django.http import JsonResponse
 
-# Create your views here.
+
+class FinderView(RetrieveModelMixin, GenericAPIView):
+    BASE_URL = "https://api.yelp.com/v3/businesses/search"
+
+    def get_yelp_url(self, location, search_term):
+        return (
+            f"{self.BASE_URL}?location={location}&term={search_term}"
+            "&sort_by=best_match&limit=10"
+        )
+
+    def get(self, request, *args, **kwargs):
+        response = requests.get(
+            self.get_yelp_url(
+                request.GET.get("location"), request.GET.get("search_term")
+            ),
+            headers={"Authorization": f"Bearer {os.getenv('YELP_API_KEY')}"},
+        ).json()
+
+        return JsonResponse(response)
